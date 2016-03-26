d14bab2700aeff39a1cc738b67f8641fa3aea5b4
Pedro Santos <github@mariodelrio.com>
2016-03-26 22:17:20 +0000

    Organize index.js

--- app/index.js
+++ app/index.js
@@ -10,70 +10,7 @@ var spawnSync = require('child_process').spawnSync;
 var request = require('request');
 var xmldom = require("xmldom");
 var wrench = require('wrench');
-
 var _0777 = parseInt('0777', 8);
-var greeting = "";
-
-fs.mkdirParent = function(dirPath, mode, callback) {
-  //Call the standard fs.mkdir
-  fs.mkdir(dirPath, mode, function(error) {
-    //When it fail in this way, do the custom steps
-    if (error && error.errno === 34) {
-      //Create all the parents recursively
-      fs.mkdirParent(path.dirname(dirPath), mode, callback);
-      //And then the directory
-      fs.mkdirParent(dirPath, mode, callback);
-    }
-    //Manually run the callback since we used our own callback to do all these
-    callback && callback(error);
-  });
-};
-
-fs.mkdirParentSync = function sync (p, opts, made) {
-    if (!opts || typeof opts !== 'object') {
-        opts = { mode: opts };
-    }
-
-    var mode = opts.mode;
-    var xfs = opts.fs || fs;
-
-    if (mode === undefined) {
-        mode = _0777 & (~process.umask());
-    }
-    if (!made) made = null;
-
-    p = path.resolve(p);
-
-    try {
-        xfs.mkdirSync(p, mode);
-        made = made || p;
-    }
-    catch (err0) {
-        switch (err0.code) {
-            case 'ENOENT' :
-                made = sync(path.dirname(p), opts, made);
-                sync(p, opts, made);
-                break;
-
-            // In the case of any other error, just see if there's a dir
-            // there already.  If so, then hooray!  If not, then something
-            // is borked.
-            default:
-                var stat;
-                try {
-                    stat = xfs.statSync(p);
-                }
-                catch (err1) {
-                    throw err0;
-                }
-                if (!stat.isDirectory()) throw err0;
-                break;
-        }
-    }
-
-    return made;
-};
-
 
 var FSharpGenerator = yeoman.generators.Base.extend({
 
@@ -90,7 +27,187 @@ var FSharpGenerator = yeoman.generators.Base.extend({
         yeoman.generators.Base.apply(this, arguments);
     },
 
-    _isOnWindows : function() {
+    init: function() {
+        this.log('Welcome to the ' + chalk.red('FSharp kata') + ' generator!');
+        this.templatedata = {};
+        var done = this.async();
+        var p = path.join(this.cacheRoot(), "sha")
+        var old = fs.existsSync(p);
+        this._getSHA(old, p, done);
+    },
+
+    askFor: function() {
+        var done = this.async();
+        var prompts = [{
+            name: 'applicationName',
+            message: 'What\'s the name of your kata?',
+            default: this.type
+        }];
+        this.prompt(prompts, function(props) {
+            this.action = 1;
+            this.templatedata.namespace = props.applicationName;
+            this.templatedata.applicationname = props.applicationName;
+            this.templatedata.guid = uuid.v4();
+            this.templatedata.packagesPath = "packages"
+            this.templatedata.paketPath = ".paket"
+            this.applicationName = props.applicationName;
+            this.type = "classlib";
+            this.paket = true;
+            this.fake = true;
+            done();
+        }.bind(this));
+    },
+
+    writing: function() {
+        if (this.action === this.ACTION_ADD_REFERENCE_TO_PROJECT)
+            return;
+
+        var log = this.log;
+        var p;
+        if (this.action === this.ACTION_CREATE_EMPTY_SOLUTION){
+            p = path.join(this._getTemplateDirectory(), 'sln')
+        }
+        else {
+            p = path.join(this._getTemplateDirectory(), this.type);
+        }
+        this._copy(p, this.applicationName);
+        if(this.paket) {
+            var bpath;
+            if(this.action !== this.ACTION_ADD_PROJECT_TO_SOLUTION) {
+                bpath = path.join(this.applicationName, ".paket", "paket.bootstrapper.exe" );
+            }
+            else {
+                bpath = path.join(".paket", "paket.bootstrapper.exe" );
+            }
+            var p = path.join(this._getTemplateDirectory(), ".paket", "paket.bootstrapper.exe");
+            this.copy(p, bpath);
+        }
+        if(this.fake) {
+            if (this.action !== this.ACTION_ADD_PROJECT_TO_SOLUTION){
+                var fakeSource = path.join(this._getTemplateDirectory(), ".fake");
+                this._copy(fakeSource, this.applicationName);
+            }
+        }
+    },
+
+    install: function() {
+        var log = this.log
+        var done = this.async();
+        var appName = this.applicationName;
+        var action = this.action;
+        var dest = this.destinationRoot();
+        var isfake = this.fake;
+        var fs = this.fs;
+
+        var generator = this;
+
+        var isWin = this._isOnWindows();
+
+        if (action === this.ACTION_ADD_REFERENCE_TO_PROJECT)
+        {
+            this._addReference(done);
+            return;
+        }
+
+        if(this.fake) {
+            if (!this._isOnWindows()) {
+                var buildShPath = path.join(dest, appName, 'build.sh');
+                var chmodProc = spawnSync('chmod', ['+x', buildShPath], {cwd: dest});
+            }
+        }
+
+        if(this.paket) {
+            var bpath;
+            if(this.action !== this.ACTION_ADD_PROJECT_TO_SOLUTION) {
+                bpath = path.join(this.applicationName, ".paket", "paket.bootstrapper.exe" );
+            }
+            else {
+                bpath = path.join(".paket", "paket.bootstrapper.exe" );
+            }
+            var bootstrapper = this._execManaged(bpath, [], {});
+
+            bootstrapper.stdout.on('data', function (data) {
+                log(data.toString());
+            });
+
+            bootstrapper.on('close', function (code) {
+                var ppath;
+                var cpath;
+                if(action !== this.ACTION_ADD_PROJECT_TO_SOLUTION) {
+                    ppath = path.join(dest, appName, ".paket", "paket.exe" );
+                    cpath = path.join(dest, appName);
+                }
+                else {
+                    ppath = path.join(dest, ".paket", "paket.exe" );
+                    cpath = dest;
+                }
+                try{
+
+                log(cpath);
+                var paket = generator._execManaged(ppath, ['convert-from-nuget','-f'], {cwd: cpath});
+
+                paket.stdout.on('data', function (data) {
+                    log(data.toString());
+                });
+
+                paket.stdout.on('close', function (data) {
+                    var simplifiy = generator._execManaged(ppath, ['simplify'], {cwd: cpath});
+
+                    simplifiy.stdout.on('data', function (data) {
+                        log(data.toString());
+                    });
+                    simplifiy.stdout.on('close', function (data) {
+                        if (isfake) {
+                            var addFake = generator._execManaged(ppath, ['add', 'nuget', 'FAKE'], {cwd: cpath});
+
+                            addFake.stdout.on('close', function(data) {
+                                done();
+                            })
+                        }
+                        else {
+                            done();
+                        }
+                    });
+                });
+                }
+                catch(ex)
+                {
+                    log(ex);
+                }
+            });
+        }
+        else
+        {
+            done();
+        }
+    },
+
+    end: function() {
+        this.log('\r\n');
+        this.log('Your project is now created');
+        this.log('\r\n');
+    },
+
+    _copy: function(dirPath, targetDirPath){
+
+        var files = fs.readdirSync(dirPath);
+        for(var i in files)
+        {
+            var f = files[i];
+            var fp = path.join(dirPath, f);
+            this.log(f);
+            if(fs.statSync(fp).isDirectory()) {
+                 var newTargetPath = path.join(targetDirPath, f);
+                 this._copy(fp, newTargetPath);
+            }
+            else {
+                var fn = path.join(targetDirPath.replace('ApplicationName', this.applicationName), f.replace('ApplicationName', this.applicationName));
+                this.template(fp,fn, this.templatedata);
+            }
+        }
+    },
+
+     _isOnWindows : function() {
         return /^win/.test(process.platform);
     },
 
@@ -158,89 +275,6 @@ var FSharpGenerator = yeoman.generators.Base.extend({
                 checkSHA(t, p, sha, old, done);
             }
         });
-    },
-
-    init: function() {
-        this.log(greeting);
-        this.log('Welcome to the ' + chalk.red('FSharp kata') + ' generator!');
-        this.templatedata = {};
-        var done = this.async();
-        var p = path.join(this.cacheRoot(), "sha")
-        var old = fs.existsSync(p);
-        this._getSHA(old, p, done);
-    },
-
-    askFor: function() {
-        var done = this.async();
-        var prompts = [{
-            name: 'applicationName',
-            message: 'What\'s the name of your kata?',
-            default: this.type
-        }];
-        this.prompt(prompts, function(props) {
-            this.action = 1;
-            this.templatedata.namespace = props.applicationName;
-            this.templatedata.applicationname = props.applicationName;
-            this.templatedata.guid = uuid.v4();
-            this.templatedata.packagesPath = "packages"
-            this.templatedata.paketPath = ".paket"
-            this.applicationName = props.applicationName;
-            this.type = "classlib";
-            this.paket = true;
-            this.fake = true;
-            done();
-        }.bind(this));
-    },
-
-    _copy: function(dirPath, targetDirPath){
-
-        var files = fs.readdirSync(dirPath);
-        for(var i in files)
-        {
-            var f = files[i];
-            var fp = path.join(dirPath, f);
-            this.log(f);
-            if(fs.statSync(fp).isDirectory()) {
-                 var newTargetPath = path.join(targetDirPath, f);
-                 this._copy(fp, newTargetPath);
-            }
-            else {
-                var fn = path.join(targetDirPath.replace('ApplicationName', this.applicationName), f.replace('ApplicationName', this.applicationName));
-                this.template(fp,fn, this.templatedata);
-            }
-        }
-    },
-
-    writing: function() {
-        if (this.action === this.ACTION_ADD_REFERENCE_TO_PROJECT)
-            return;
-
-        var log = this.log;
-        var p;
-        if (this.action === this.ACTION_CREATE_EMPTY_SOLUTION){
-            p = path.join(this._getTemplateDirectory(), 'sln')
-        }
-        else {
-            p = path.join(this._getTemplateDirectory(), this.type);
-        }
-        this._copy(p, this.applicationName);
-        if(this.paket) {
-            var bpath;
-            if(this.action !== this.ACTION_ADD_PROJECT_TO_SOLUTION) {
-                bpath = path.join(this.applicationName, ".paket", "paket.bootstrapper.exe" );
-            }
-            else {
-                bpath = path.join(".paket", "paket.bootstrapper.exe" );
-            }
-            var p = path.join(this._getTemplateDirectory(), ".paket", "paket.bootstrapper.exe");
-            this.copy(p, bpath);
-        }
-        if(this.fake) {
-            if (this.action !== this.ACTION_ADD_PROJECT_TO_SOLUTION){
-                var fakeSource = path.join(this._getTemplateDirectory(), ".fake");
-                this._copy(fakeSource, this.applicationName);
-            }
-        }
     },
 
     _getProjectFile: function() {
@@ -378,104 +412,67 @@ var FSharpGenerator = yeoman.generators.Base.extend({
             done();
         });
     },
-
-    install: function() {
-        var log = this.log
-        var done = this.async();
-        var appName = this.applicationName;
-        var action = this.action;
-        var dest = this.destinationRoot();
-        var isfake = this.fake;
-        var fs = this.fs;
-
-        var generator = this;
-
-        var isWin = this._isOnWindows();
-
-        if (action === this.ACTION_ADD_REFERENCE_TO_PROJECT)
-        {
-            this._addReference(done);
-            return;
-        }
-
-        if(this.fake) {
-            if (!this._isOnWindows()) {
-                var buildShPath = path.join(dest, appName, 'build.sh');
-                var chmodProc = spawnSync('chmod', ['+x', buildShPath], {cwd: dest});
-            }
-        }
-
-        if(this.paket) {
-            var bpath;
-            if(this.action !== this.ACTION_ADD_PROJECT_TO_SOLUTION) {
-                bpath = path.join(this.applicationName, ".paket", "paket.bootstrapper.exe" );
-            }
-            else {
-                bpath = path.join(".paket", "paket.bootstrapper.exe" );
-            }
-            var bootstrapper = this._execManaged(bpath, [], {});
-
-            bootstrapper.stdout.on('data', function (data) {
-                log(data.toString());
-            });
-
-            bootstrapper.on('close', function (code) {
-                var ppath;
-                var cpath;
-                if(action !== this.ACTION_ADD_PROJECT_TO_SOLUTION) {
-                    ppath = path.join(dest, appName, ".paket", "paket.exe" );
-                    cpath = path.join(dest, appName);
-                }
-                else {
-                    ppath = path.join(dest, ".paket", "paket.exe" );
-                    cpath = dest;
-                }
-                try{
-
-                log(cpath);
-                var paket = generator._execManaged(ppath, ['convert-from-nuget','-f'], {cwd: cpath});
-
-                paket.stdout.on('data', function (data) {
-                    log(data.toString());
-                });
-
-                paket.stdout.on('close', function (data) {
-                    var simplifiy = generator._execManaged(ppath, ['simplify'], {cwd: cpath});
-
-                    simplifiy.stdout.on('data', function (data) {
-                        log(data.toString());
-                    });
-                    simplifiy.stdout.on('close', function (data) {
-                        if (isfake) {
-                            var addFake = generator._execManaged(ppath, ['add', 'nuget', 'FAKE'], {cwd: cpath});
-
-                            addFake.stdout.on('close', function(data) {
-                                done();
-                            })
-                        }
-                        else {
-                            done();
-                        }
-                    });
-                });
-                }
-                catch(ex)
-                {
-                    log(ex);
-                }
-            });
-        }
-        else
-        {
-            done();
-        }
-    },
-
-    end: function() {
-        this.log('\r\n');
-        this.log('Your project is now created');
-        this.log('\r\n');
-    }
 });
+
+//Helper functions
+fs.mkdirParent = function(dirPath, mode, callback) {
+  //Call the standard fs.mkdir
+  fs.mkdir(dirPath, mode, function(error) {
+    //When it fail in this way, do the custom steps
+    if (error && error.errno === 34) {
+      //Create all the parents recursively
+      fs.mkdirParent(path.dirname(dirPath), mode, callback);
+      //And then the directory
+      fs.mkdirParent(dirPath, mode, callback);
+    }
+    //Manually run the callback since we used our own callback to do all these
+    callback && callback(error);
+  });
+};
+
+fs.mkdirParentSync = function sync (p, opts, made) {
+    if (!opts || typeof opts !== 'object') {
+        opts = { mode: opts };
+    }
+
+    var mode = opts.mode;
+    var xfs = opts.fs || fs;
+
+    if (mode === undefined) {
+        mode = _0777 & (~process.umask());
+    }
+    if (!made) made = null;
+
+    p = path.resolve(p);
+
+    try {
+        xfs.mkdirSync(p, mode);
+        made = made || p;
+    }
+    catch (err0) {
+        switch (err0.code) {
+            case 'ENOENT' :
+                made = sync(path.dirname(p), opts, made);
+                sync(p, opts, made);
+                break;
+
+            // In the case of any other error, just see if there's a dir
+            // there already.  If so, then hooray!  If not, then something
+            // is borked.
+            default:
+                var stat;
+                try {
+                    stat = xfs.statSync(p);
+                }
+                catch (err1) {
+                    throw err0;
+                }
+                if (!stat.isDirectory()) throw err0;
+                break;
+        }
+    }
+
+    return made;
+};
 
 module.exports = FSharpGenerator;
